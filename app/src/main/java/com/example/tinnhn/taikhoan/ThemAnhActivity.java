05e9811cac696dc286f598d1ac08d130076a0c94
PQT123 <pqtruong123@gmail.com>
2020-11-19 18:17:21 +0700

    cập nhật taikhoan 2%

--- app/src/main/java/com/example/tinnhn/taikhoan/ThemAnhActivity.java
+++ app/src/main/java/com/example/tinnhn/taikhoan/ThemAnhActivity.java
@@ -1,19 +1,95 @@
 package com.example.tinnhn.taikhoan;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.tinnhn.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Calendar;
 
 public class ThemAnhActivity extends AppCompatActivity {
 
-    FirebaseStorage storage = FirebaseStorage.getInstance();
+//    FirebaseStorage storage = FirebaseStorage.getInstance();
+    // Get a non-default Storage bucket
+    FirebaseStorage storage = FirebaseStorage.getInstance("gs://duan1-f124f.appspot.com");
+    ImageView ivHinh;
+    Button btnThem;
+    int REQUEST_CODE_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_anh);
+
+        StorageReference storageRef = storage.getReference();
+        ivHinh = findViewById(R.id.ivHinh);
+        ivHinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE_IMAGE);
+            }
+        });
+        btnThem = findViewById(R.id.btnThem);
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                StorageReference mountainsRef = storageRef.child("hinhanh_"+calendar.getTimeInMillis());
+
+// Get the data from an ImageView as bytes
+                ivHinh.setDrawingCacheEnabled(true);
+                ivHinh.buildDrawingCache();
+                Bitmap bitmap = ((BitmapDrawable) ivHinh.getDrawable()).getBitmap();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                byte[] data = baos.toByteArray();
+                UploadTask uploadTask = mountainsRef.putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        Toast.makeText(ThemAnhActivity.this, "NOT OK ", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                        // ...
+//                        Uri uri= taskSnapshot.getMetadata();
+                        StorageMetadata metadata =  taskSnapshot.getMetadata();
+                        Toast.makeText(ThemAnhActivity.this, "OK: "+metadata, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK && data != null){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            ivHinh.setImageBitmap(bitmap);
+        }
     }
 }
