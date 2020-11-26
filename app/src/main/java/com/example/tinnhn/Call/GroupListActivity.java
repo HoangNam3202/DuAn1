4b681271253fca7157576243666cb871ca61d419
Tuan0304 <tranminhshuan0304@gmail.com>
2020-11-26 13:06:25 +0700

    group list

--- app/src/main/java/com/example/tinnhn/Call/GroupListActivity.java
+++ app/src/main/java/com/example/tinnhn/Call/GroupListActivity.java
@@ -38,15 +38,17 @@ public class GroupListActivity extends AppCompatActivity {
         final EditText tengrp=findViewById(R.id.tengroup);
         Button them=findViewById(R.id.themgrp);
         ListView grplist=findViewById(R.id.grplv);
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
         ArrayList<String> dsgrp = new ArrayList<>();
 
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, dsgrp);
+        grplist.setAdapter(adapter);
         mDatabase.child("Group").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 String nhom=snapshot.getValue().toString();
                 dsgrp.add(nhom);
+                adapter.notifyDataSetChanged();
             }
 
             @Override
@@ -70,16 +72,16 @@ public class GroupListActivity extends AppCompatActivity {
             }
         });
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, dsgrp);
-        grplist.setAdapter(adapter);
-
-
 
         them.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Group group =new Group(tengrp.getText().toString(),RandomString(9));
-                mDatabase.child("Group").push().setValue(group);
+//                Group group =new Group(tengrp.getText().toString(),RandomString(9));
+//                mDatabase.child("Group").push().setValue(group);
+
+
+
+
             }
         });
 
--- app/src/main/java/com/example/tinnhn/ui/main/GroupFragment.java
+++ app/src/main/java/com/example/tinnhn/ui/main/GroupFragment.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,17 +17,72 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.example.tinnhn.R;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class GroupFragment extends Fragment {
 
     private View mRoot;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+    private DatabaseReference mDatabase;
+    String TenGroup;
+    int IdGroup;
+
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mRoot = inflater.inflate(R.layout.fragment_main2,container,false);
+        mRoot = inflater.inflate(R.layout.group_list,container,false);
 
+       final EditText tengrp=mRoot.findViewById(R.id.tengroup);
+       Button them=mRoot.findViewById(R.id.themgrp);
+        ListView grplist=mRoot.findViewById(R.id.grplv);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        ArrayList<String> dsgrp = new ArrayList<>();
+
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, dsgrp);
+        grplist.setAdapter(adapter);
+        mDatabase.child("Group").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String nhom=snapshot.getValue().toString();
+                dsgrp.add(nhom);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        them.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "có cái gì đâu mà ấn :)))", Toast.LENGTH_SHORT).show();
+            }
+        });
         return mRoot;
     }
 }
