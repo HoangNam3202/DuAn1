239491ea538573ee0e825a5775c5fdecb77e16de
Tuan0304 <tranminhshuan0304@gmail.com>
2020-11-26 16:58:12 +0700

    xong phan group mess

---
+++ app/src/main/java/com/example/tinnhn/Call/GroupAdapter.java
@@ -0,0 +1,94 @@
+package com.example.tinnhn.Call;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+
+import com.example.tinnhn.HoiThoai;
+import com.example.tinnhn.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class GroupAdapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private List<Group> hoiThoaiList;
+    private DatabaseReference mDatabase;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    public GroupAdapter(Context context, int layout, List<Group> hoiThoaiList) {
+        this.context = context;
+        this.layout = layout;
+        this.hoiThoaiList = hoiThoaiList;
+    }
+
+
+    @Override
+    public int getCount() {
+        return hoiThoaiList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(layout, null);
+
+
+        TextView tvUser_tin_nhan = view.findViewById(R.id.tvHoiThoai_User_tin_nhan);
+        TextView tv_HoiThoaiBanCuaUser = view.findViewById(R.id.tv_HoiThoaiBanCuaUser);
+        ImageView imgAnh_User_tin_nhan = view.findViewById(R.id.imgAnh_User_tin_nhan);
+        ImageView imgAnh_Ban_Cua_User = view.findViewById(R.id.imgAnh_Ban_Cua_User);
+        CardView card_view_Friend = view.findViewById(R.id.card_view);
+        CardView card_view_User = view.findViewById(R.id.card_view1);
+
+        Group group = hoiThoaiList.get(i);
+        Intent intent = ((Activity) context).getIntent();
+
+        sharedPreferences = context.getSharedPreferences("GhiNhoDangNhap", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        String EmailNguoiGui = intent.getStringExtra("EmailNguoiGui");
+        final String EmailUser = sharedPreferences.getString("tenTaiKhoan", "");
+
+        if (group.Email.equals(EmailUser)) {
+            tvUser_tin_nhan.setText(group.message);
+            tv_HoiThoaiBanCuaUser.setVisibility(View.GONE);
+            imgAnh_Ban_Cua_User.setVisibility(View.GONE);
+            card_view_Friend.setVisibility(View.GONE);
+        }
+        if (!group.Email.equals(EmailUser)) {
+            tv_HoiThoaiBanCuaUser.setText(group.message);
+            tvUser_tin_nhan.setVisibility(View.GONE);
+            imgAnh_User_tin_nhan.setVisibility(View.GONE);
+            card_view_User.setVisibility(View.GONE);
+        }
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/tinnhn/ui/main/GroupFragment.java
+++ app/src/main/java/com/example/tinnhn/ui/main/GroupFragment.java
@@ -1,9 +1,12 @@
 package com.example.tinnhn.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +18,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.tinnhn.Call.GroupHoiThoaiActivity;
 import com.example.tinnhn.R;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.database.ChildEventListener;
@@ -43,6 +47,8 @@ public class GroupFragment extends Fragment {
 
        final EditText tengrp=mRoot.findViewById(R.id.tengroup);
        Button them=mRoot.findViewById(R.id.themgrp);
+
+
         ListView grplist=mRoot.findViewById(R.id.grplv);
         mDatabase = FirebaseDatabase.getInstance().getReference();
         ArrayList<String> dsgrp = new ArrayList<>();
@@ -74,6 +80,51 @@ public class GroupFragment extends Fragment {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        grplist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+              String tenkey= dsgrp.get(position);
+                Intent i = new Intent(getActivity(), GroupHoiThoaiActivity.class);
+                        i.putExtra("idgroup", tenkey);
+                        startActivity(i);
+//                Toast.makeText(getActivity(), tenkey, Toast.LENGTH_SHORT).show();
+//                mDatabase.child("Group").addChildEventListener(new ChildEventListener() {
+//                    @Override
+//                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//                        String key = snapshot.getValue().toString();
+//                        for (int i=0;i<=dsgrp.size();i++){
+//                            if(dsgrp.get(position).equals(key)){
+//                                String key2=snapshot.getKey();
+//                                Toast.makeText(getActivity(), key2, Toast.LENGTH_SHORT).show();
+//
+//                            }
+//                        }
+////
+//                    }
+//
+//                    @Override
+//                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//
+//                    }
+//                });
 
             }
         });
