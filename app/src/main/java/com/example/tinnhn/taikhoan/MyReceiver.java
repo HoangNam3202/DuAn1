283078ba0dfd2066e452da7a3abe39212d97084e
HoangNam1101 <namhhpk01637@fpt.edu.vn>
2020-12-01 20:21:28 +0700

    fix check mang. tren may that

--- app/src/main/java/com/example/tinnhn/taikhoan/MyReceiver.java
+++ app/src/main/java/com/example/tinnhn/taikhoan/MyReceiver.java
@@ -12,16 +12,6 @@ import com.example.tinnhn.MainActivity;
 public class MyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        final String action = intent.getAction();
-        if (action.equals(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION)) {
-            if (intent.getBooleanExtra(WifiManager.EXTRA_SUPPLICANT_CONNECTED, false)) {
-                kTraMang = 1;
-                intent = new Intent(context,LoginActivity.class);
-                context.sendBroadcast(intent);
-            }
-            else {
-                kTraMang = 0;
-            }
-        }
+        kTraMang = CheckInternet.getConnectivityStatusString(context);
     }
 }
